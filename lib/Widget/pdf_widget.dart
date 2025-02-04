b5b5e61294344fe8a0dc342ebc138bf64f8d0150
Pemtat <pemtat.ruk@gmail.com>
2025-02-04 17:15:12 +0700

    pdf download

--- lib/Widget/pdf_widget.dart
+++ lib/Widget/pdf_widget.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:toyotamobile/Function/gettoken.dart';
 import 'package:toyotamobile/Function/ticketdata.dart';
-import 'package:toyotamobile/Styles/text.dart';
 import 'package:toyotamobile/Widget/loadingcircle_widget.dart';
 import 'package:toyotamobile/Widget/pdfviewer_widget.dart';
 
@@ -48,9 +51,40 @@ class PdfFile extends StatelessWidget {
               ),
             ),
           ),
-          Text('View PDF', style: TextStyleList.subtext3),
+          // Text('View PDF', style: TextStyleList.subtext3),
+          // SizedBox(height: 10),
+          // ElevatedButton(
+          //   onPressed: () async {
+          //     String? token = await getToken();
+          //     showDialog(
+          //         context: context,
+          //         barrierColor: Color.fromARGB(59, 0, 0, 0),
+          //         barrierDismissible: false,
+          //         builder: (BuildContext context) {
+          //           return const Center(child: DataCircleLoading());
+          //         });
+          //     await fetchPdfReport(path, token ?? '', pdfReport, option);
+          //     Navigator.pop(context);
+          //     if (pdfReport.value.isNotEmpty) {
+          //       XFile pdfFile = await base64ToPdfFile(pdfReport.value, name);
+          //       Share.shareXFiles([pdfFile]);
+          //     }
+          //   },
+          //   child: Text('แชร์'),
+          // ),
         ],
       ),
     );
+  }
+
+  Future<XFile> base64ToPdfFile(String base64String, String fileName) async {
+    final bytes = base64Decode(base64String);
+    final directory = await getApplicationDocumentsDirectory();
+    final filePath = '${directory.path}/$fileName.pdf';
+    final file = File(filePath);
+
+    await file.writeAsBytes(bytes);
+
+    return XFile(filePath);
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -16,6 +16,7 @@ import geolocator_apple
 import package_info_plus
 import path_provider_foundation
 import pdfx
+import share_plus
 import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
@@ -32,6 +33,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PdfxPlugin.register(with: registry.registrar(forPlugin: "PdfxPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
